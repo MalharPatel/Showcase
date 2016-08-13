ba5a733a8274c68c125fcbf294e49df2b55f50ae
MalharPatel <malharpatel97@gmail.com>
2016-08-12 17:53:44 -0700

    initial UI for login

---
+++ Showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Showcase
+//
+//  Created by Malhar Patel on 8/12/16.
+//  Copyright © 2016 Malhar Patel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
